cda6533ead3462c99712be432b7f8546604b4822
briansomogyi <brian.somogyi@emanuel.ro>
2023-11-06 09:53:50 +0200

    Done some changes

---
+++ InstanceVSStatic/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceVSStatic
+{
+    public class Person
+    {
+        public string FirstName = "John";
+        public string LastName = "Doe";
+        public static int PopulationCount = 0;
+
+    }
+}
---
+++ InstanceVSStatic/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InstanceVSStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.FirstName); Console.WriteLine(person.LastName);
+            Console.WriteLine(Person.PopulationCount);
+        }
+    }
+}
